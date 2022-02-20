f76365efc8e8ccbc341873203fe53d4d80cd97c3
Shawket4 <95412838+Shawket4@users.noreply.github.com>
2022-02-20 12:23:53 +0200

    Add files via upload

---
+++ precache-manifest.0f49afaabe074cdcebba1f72896988a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f33f36f13f647b0315b6936d09b5d8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1c949818015d8385b55",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "ebb8e8906fe022d10f2c",
+    "url": "/static/css/main.c6d129bb.chunk.css"
+  },
+  {
+    "revision": "b1c949818015d8385b55",
+    "url": "/static/js/2.0ceaf747.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.0ceaf747.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebb8e8906fe022d10f2c",
+    "url": "/static/js/main.b226afe6.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
